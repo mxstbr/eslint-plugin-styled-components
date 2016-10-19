9db9a5256b1540af59b6aa69245fe7068d7d4e98
Max <contact@mstoiber.com>
2016-10-19 19:48:43 +0200

    Stuck

--- rules/lint-css.js
+++ rules/lint-css.js
@@ -1,7 +1,25 @@
-module.exports = function(context) {
-  return {
-		TaggedTemplateExpression(node) {
-			
-		}
-	};
-};
+const stylelint = require('stylelint')
+
+const helperMethods = ['keyframes', 'injectGlobal', 'css']
+
+const isHelper = (node) => node.tag.type === 'Identifier' && helperMethods.includes(node.tag.name)
+
+const isStyledTagname = (node) => node.tag.type === 'MemberExpression' && node.tag.object.name === 'styled' && node.tag.property.type === 'Identifier'
+
+module.exports = {
+	create(context) {
+		const options = context.options[0]
+
+	  return {
+			TaggedTemplateExpression(node) {
+				if(isStyledTagname(node)) {
+					// This doesn't work because this function _needs_ to return sync :(
+					stylelint.lint({
+						code: node.quasi.quasis[0].value.raw,
+						config: options.stylelint,
+					}).then().catch()
+				}
+			}
+		};
+	},
+}
